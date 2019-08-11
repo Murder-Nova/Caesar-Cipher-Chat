773a5a141edf7399b6ae8d4552784e131afa4301
Murder-Nova <sebastian.correa1@correo.icesi.edu.co>
2019-08-11 16:24:49 -0500

    Server class modifications added

--- src/model/Servidor.java
+++ src/model/Servidor.java
@@ -5,27 +5,27 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Vector;
 
 public class Servidor {
 
-	/**
-	 * Puerto por donde el servidor atendera a los clientes
-	 */
+	// Puerto por donde el servidor atendera a los clientes
 	public static final int PORT = 8000;
-	/**
-	 * El servidor dispone de un serversocket, para permitir la conexion a los
-	 * clientes
-	 */
+
+	// Vector para almacenar a los clientes activos
+	static Vector<Regulador> activeClients = new Vector<>();
+
+	// El servidor dispone de un serversocket, para permitir la conexion a los clientes
 	private static ServerSocket serverSocket;
-	/**
-	 * El servidor dispone de un socket para atender a cada cliente por individual
-	 */
+
+	// El servidor dispone de un socket para atender a cada cliente por individual
 	private static Socket socket;
 
-	/**
-	 * Llave usada para cifrar el mensaje del cliente
-	 */
+	// Llave usada para cifrar el mensaje del cliente
 	private static int cipherKey = (int) Math.random() * 21;
+
+	// Contador de clientes
+	static int count = 0;
 
 	public static void main(String[] args) {
 
@@ -42,12 +42,17 @@ public class Servidor {
 				System.out.println("El cliente se ha conectado!");
 				in = new DataInputStream(socket.getInputStream());
 				out = new DataOutputStream(socket.getOutputStream());
-				String mensajeObtenidoCliente = in.readUTF();
-				System.out.println("El mensaje enviado por el cliente fue : " + mensajeObtenidoCliente);
-				String respuestaServer = metodoServicioServer(mensajeObtenidoCliente);
-				out.writeUTF(respuestaServer);
-				socket.close();
-				System.out.println("::El cliente fue desconectado del server::");
+
+				// Se crea un nuevo regulador que se encargara de la solicitud del nuevo cliente
+				Regulador cliente = new Regulador(socket, "Cliente " + count, in, out);
+
+				// Se crea un hilo para permitir la conexion de varios clientes a la vez
+				Thread hiloCliente = new Thread(cliente);
+
+				// Se agrega el cliente a la lista de clientes activos
+				activeClients.add(cliente);
+				hiloCliente.start();
+				count++;
 
 			}
 
@@ -56,13 +61,6 @@ public class Servidor {
 			e.printStackTrace();
 		}
 
-	}
-
-	private static String metodoServicioServer(String mensajeObtenidoCliente) {
-
-		String[] cadena = mensajeObtenidoCliente.split(";");
-
-		return Integer.parseInt(cadena[0]) + Integer.parseInt(cadena[1]) + "";
 	}
 
 	/**
@@ -115,57 +113,5 @@ public class Servidor {
 		}
 
 		return cipherText;
-	}
-
-	/**
-	 * Este método se encarga de descifrar la palabra recibida utilizando cifrado Cesar inverso
-	 * 
-	 * @param cipherText  es la palabra que se desea cifrar, enviada por un cliente
-	 * @param shift es la llave de cifrado entre 1 y 20 que se va a utilizar, el
-	 *              número de posiciones que se mueven
-	 * @return retorna un String con la palabra descifrada
-	 */
-	private static String descifrarCesar(String cipherText, int shift) {
-		//La palabra descifrada que se retornará después
-		String decryptedText = "";
-		// Se utiliza una variable tipo char para cifrar la palabra letra por letra
-		char letter;
-
-		for (int i = 0; i < cipherText.length(); i++) {
-			letter = cipherText.charAt(i);
-
-			// Primero se comprueba si la letra en la posición i está en minúscula
-			if (letter >= 'a' && letter <= 'z') {
-				// Se corre de posición hacia la izquierda la letra según la llave de cifrado
-				letter = (char) (letter - shift);
-
-				// Si el valor en ASCII de letter es menor a 'a' significa que se desbordó del abecedario en minúscula
-				// Por consiguiente, se debe reiniciar la cuenta, haciendo que 'z' sea anterior a 'a'
-				if (letter < 'a') {
-					letter = (char) (letter - 'a' + 'z' + 1);
-				}
-				cipherText = cipherText + letter;
-			}
-
-			// Se comprueba si la letra en la posición i está en mayúscula
-			else if (letter >= 'A' && letter <= 'Z') {
-				// Se corre de posición hacia la izquierda la letra según la llave de cifrado
-				letter = (char) (letter - shift);
-
-				// Si el valor en ASCII de letter es menor a 'A' significa que se desbordó del abecedario en mayúscula
-				// Por consiguiente, se debe reiniciar la cuenta, haciendo que 'Z' sea anterior a 'A'
-				if (letter < 'A') {
-					letter = (char) (letter - 'A' + 'Z' + 1);
-				}
-				cipherText = cipherText + letter;
-			}
-
-			// Si letter no es una letra del abecedario, entonces simplemente se concatena a la palabra descifrada
-			else {
-				cipherText = cipherText + letter;
-			}
-		}
-
-		return decryptedText;
 	}
 }
